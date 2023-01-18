bd7f1e0e4c8aecd0bb49c7a026e8182ee2f0825c
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-01-18 16:02:38 -0500

    add more routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
+const signupRoutes = require('/signupRoutes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
+router.use('/signup', signupRoutes);
 
 module.exports = router;
