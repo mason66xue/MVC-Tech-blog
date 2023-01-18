357dff490cc7c5534009e86cc2e695da21a164de
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-01-18 15:56:11 -0500

    update in routes.js

---
+++ controllers/api/userRoute.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+const {User} = require('../../models'); //object destructuring
+
+console.log("User:", User);
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+        req.session.save(() => {
+
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+router.post('/login', async (req, res) => {
+    console.log(req.body)
+    // console.log('email', req.body.email)
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+        console.log('email', req.body.email)
+        if (!userData) {
+            console.log('invalid user email')
+            res.status(500).json({
+                message: 'Incorrect email or password, please try again'
+            });
+            return;
+        }
+        console.log('email was found successfully')
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            console.log("invalid password");
+            res.status(500).json({
+                message: 'Incorrect email or password, please try again'
+            });
+            return;
+        }
+        console.log('password', req.body.password)
+        console.log('password check complete')
+        req.session.save(() => {
+
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+});
+
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(400).end();
+    }
+});
+
+
+router.get('/profile')
+
+module.exports = router;
+
+router.post('./')
